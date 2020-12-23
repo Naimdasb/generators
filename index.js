56f10af25396ba42a010fb09ebedd09770339f2b
naimdasb <naimdasb@gmail.com>
2020-12-23 13:03:43 -0300

    generator coutner

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function* generator() {
+/*function* generator() {
     yield 'Hello'
     yield 'There'
     yield 'Friend'
@@ -10,5 +10,24 @@ console.log(gen.next())
 console.log(gen.next())
 console.log(gen.next())
 console.log(gen.next())
+*/
+
+function* generator(add) {
+    while (add < 6) {
+        yield add++
+    }
+
+}
+
+const gen = generator(0)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+
+
+
 
 
